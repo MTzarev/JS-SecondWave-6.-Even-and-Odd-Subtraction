f4457b82ae3ef94daa369618fe058420a05ecb10
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-23 22:57:31 +0300

    Add files via upload

---
+++ 6. Even and Odd Subtraction.js
@@ -0,0 +1,17 @@
+function evenAndOddSubtraction(arr) {
+    let evenSum = 0;
+    let oddSum = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] = Number(arr[i]);
+    }
+    for (num of arr) {
+        if (num % 2 === 0) {
+            evenSum += num;
+        } else {
+            oddSum += num;
+        }
+    }
+    let diff = evenSum - oddSum;
+    console.log(diff);
+}
+evenAndOddSubtraction([3, 5, 7, 9]);
